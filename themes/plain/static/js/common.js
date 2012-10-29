1864e5649cb18b4acf457216b7197fdc82cf85a8
Nick Vlku <nick.vlku@gmail.com>
2012-10-29 13:47:43 -0700

    Converting datetime correctly for local time

--- themes/plain/static/js/common.js
+++ themes/plain/static/js/common.js
@@ -1,5 +1,7 @@
 localize_time = function(status_date) {
-    local_time = new Date(status_date*1000).toString('hh:mm:ss tt dddd, MMMM dd, yyyy');
+    d = new Date(0)
+    d.setUTCSeconds(status_date*1000)
+    local_time = d.toString('hh:mm:ss tt dddd, MMMM dd, yyyy');
     if (local_time.indexOf("00:") == 0) {
         s = local_time.split(":")
         s[0] = "12"
